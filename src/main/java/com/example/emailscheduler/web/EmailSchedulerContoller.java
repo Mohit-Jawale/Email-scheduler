006b423034b8c2865d4fdeb26eaa68480f71da44
Akshata-AB <akshatabhonsle27@gmail.com>
2023-11-19 23:35:33 -0800

    Added jobs list view

--- src/main/java/com/example/emailscheduler/web/EmailSchedulerContoller.java
+++ src/main/java/com/example/emailscheduler/web/EmailSchedulerContoller.java
@@ -6,31 +6,37 @@ import com.example.emailscheduler.quartz.job.EmailJob;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
+import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@RestController
+@Controller
 public class EmailSchedulerContoller {
 
     @Autowired
     private Scheduler scheduler;
 
     @PostMapping("/schedule/email")
-    public ResponseEntity<EmailResponse> scheduleEmail(@Valid  @RequestBody @ModelAttribute EmailRequest emailRequest) {
+    public String scheduleEmail(@Valid  @RequestBody @ModelAttribute EmailRequest emailRequest) {
         try {
             ZonedDateTime dateTime = ZonedDateTime.of(emailRequest.getDateTime(), emailRequest.getTimeZone());
 
             if(dateTime.isBefore(ZonedDateTime.now())) {
                 EmailResponse emailResponse = new EmailResponse(false,
                         "dateTime must be after current time");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(emailResponse);
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(emailResponse);
+                return "redirect:/jobs";
             }
 
             JobDetail jobDetail = buildJobDetail(emailRequest);
@@ -39,19 +45,37 @@ public class EmailSchedulerContoller {
 
             EmailResponse emailResponse = new EmailResponse(true,
                     jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
-            return ResponseEntity.ok(emailResponse);
+//            return ResponseEntity.ok(emailResponse);
+            return "redirect:/jobs";
         } catch (SchedulerException ex) {
             log.error("Error scheduling email", ex);
 
             EmailResponse emailResponse = new EmailResponse(false,
                     "Error scheduling email. Please try later!");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(emailResponse);
+            return "redirect:/jobs";
         }
     }
     @GetMapping("/get")
     public ResponseEntity<String> getAPiTest(){
         return ResponseEntity.ok("Get ApiTest-Pass");
     }
+
+    @GetMapping("/jobs")
+    public String getScheduleEmails(Model model) throws SchedulerException {
+
+        List<JobDataMap> jobs = new ArrayList<>();
+
+        for (String groupName : scheduler.getJobGroupNames()) {
+
+            for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
+                jobs.add(scheduler.getJobDetail(jobKey).getJobDataMap());
+            }
+        }
+        model.addAttribute("jobs", jobs);
+        return "jobs";
+      // return ResponseEntity.ok(jobs);
+    }
+
     private JobDetail buildJobDetail(EmailRequest scheduleEmailRequest) {
         JobDataMap jobDataMap = new JobDataMap();
 

70818f08379f8a9900fbd88a550f8b82eda0031e
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-11-22 09:33:44 -0800

    Add files via upload

--- src/main/java/com/example/emailscheduler/web/EmailSchedulerContoller.java
+++ src/main/java/com/example/emailscheduler/web/EmailSchedulerContoller.java
@@ -6,37 +6,88 @@ import com.example.emailscheduler.quartz.job.EmailJob;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
-import org.quartz.impl.matchers.GroupMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 @Slf4j
-@Controller
+@RestController
 public class EmailSchedulerContoller {
 
     @Autowired
     private Scheduler scheduler;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(EmailJob.class);
+
+    public static List<String> extractEmails(List<String> inputStrings) {
+        List<String> extractedEmails = new ArrayList<>();
+        String emailRegex = "[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}";
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        for (String str : inputStrings) {
+            Matcher matcher = pattern.matcher(str);
+            while (matcher.find()) {
+                extractedEmails.add(matcher.group());
+            }
+        }
+
+        return extractedEmails;
+    }
+
+    @GetMapping("/Recipient")
+    public  List<Recipient> getAllEmails(List<String> names){
+
+        StringJoiner sj = new StringJoiner("','", "'", "'");
+        for (String name : names) {
+            sj.add(name);
+        }
+        String sql = "SELECT * FROM recipient WHERE name IN (" + sj.toString() + ")";
+        logger.info(sql);
+        List<Recipient> recipients =  jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Recipient.class));
+
+        return  recipients;
+
+    }
+
 
     @PostMapping("/schedule/email")
-    public String scheduleEmail(@Valid  @RequestBody @ModelAttribute EmailRequest emailRequest) {
+    public ResponseEntity<EmailResponse> scheduleEmail(@Valid  @RequestBody @ModelAttribute EmailRequest emailRequest) {
         try {
+            List<String> emails = Arrays.asList(emailRequest.getEmail().split(","));
+
+            List<String> onlyEmails = extractEmails(emails);
+            logger.info(onlyEmails.toString());
+            List<Recipient> response = getAllEmails(emails);
+            List<String> myList = new ArrayList<>();
+
+            for (Recipient r : response)
+            {
+                myList.add(r.getEmail_address());
+            }
+            myList.addAll(onlyEmails);
+            String result = String.join(",", myList);
+            logger.info("This is result-:"+result);
+            emailRequest.setEmail(result);
+
+
+
             ZonedDateTime dateTime = ZonedDateTime.of(emailRequest.getDateTime(), emailRequest.getTimeZone());
 
             if(dateTime.isBefore(ZonedDateTime.now())) {
                 EmailResponse emailResponse = new EmailResponse(false,
                         "dateTime must be after current time");
-//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(emailResponse);
-                return "redirect:/jobs";
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(emailResponse);
             }
 
             JobDetail jobDetail = buildJobDetail(emailRequest);
@@ -45,37 +96,19 @@ public class EmailSchedulerContoller {
 
             EmailResponse emailResponse = new EmailResponse(true,
                     jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
-//            return ResponseEntity.ok(emailResponse);
-            return "redirect:/jobs";
+            return ResponseEntity.ok(emailResponse);
         } catch (SchedulerException ex) {
             log.error("Error scheduling email", ex);
 
             EmailResponse emailResponse = new EmailResponse(false,
                     "Error scheduling email. Please try later!");
-            return "redirect:/jobs";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(emailResponse);
         }
     }
     @GetMapping("/get")
     public ResponseEntity<String> getAPiTest(){
         return ResponseEntity.ok("Get ApiTest-Pass");
     }
-
-    @GetMapping("/jobs")
-    public String getScheduleEmails(Model model) throws SchedulerException {
-
-        List<JobDataMap> jobs = new ArrayList<>();
-
-        for (String groupName : scheduler.getJobGroupNames()) {
-
-            for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
-                jobs.add(scheduler.getJobDetail(jobKey).getJobDataMap());
-            }
-        }
-        model.addAttribute("jobs", jobs);
-        return "jobs";
-      // return ResponseEntity.ok(jobs);
-    }
-
     private JobDetail buildJobDetail(EmailRequest scheduleEmailRequest) {
         JobDataMap jobDataMap = new JobDataMap();
 

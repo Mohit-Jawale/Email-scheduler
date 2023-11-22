42c2596833bf6cdb9fc2a0be8f6d8218b1e59a62
Mohit-Jawale <61145739+Mohit-Jawale@users.noreply.github.com>
2023-11-22 09:38:06 -0800

    Add files via upload

---
+++ src/main/java/com/example/emailscheduler/repository/Recipient.java
@@ -0,0 +1,39 @@
+package com.example.emailscheduler.web;
+
+
+public class Recipient {
+    private String name;
+    private String email_address;
+    private String contact;
+
+    // Getter for the name field
+    public String getName() {
+        return name;
+    }
+
+    // Setter for the name field
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for the email field
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    // Setter for the email field
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    // Getter for the contact field
+    public String getContact() {
+        return contact;
+    }
+
+    // Setter for the contact field
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
+
